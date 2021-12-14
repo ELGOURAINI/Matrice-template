a9d4bc26471bf796ce58024c233dd5db8bb7c24e
OUMAIMA EL GOURAINI <oumaimasouad08@gmail.com>
2021-12-14 15:00:06 +0100

    innovate

--- StaticLib_matrix/MATRICE.h
+++ StaticLib_matrix/MATRICE.h
@@ -1,22 +1,160 @@
 #pragma once
+#include "pch.h"
+#include "MATRICE.h"
+#include <iostream>
+#include "assert.h"
+using namespace std;
 namespace algebre
 {
+	template <class M>
 	class Matrice
 	{
 	private:
 		int nbre_lgn;
 		int nbre_col;
-		int** tab;
+		M** tab;
 
 	public:
-		Matrice(int lg,int col);
-		void remplissage(int val);
+		Matrice(int lg, int col);
+		void remplissage(M val);
 		void remplir();
-		Matrice& operator+(const Matrice& M);
-		Matrice& operator-(const Matrice& M);
-		Matrice& operator*(int d);
-		Matrice& operator*(Matrice& M);
+		Matrice<M>& operator+(const Matrice<M>& M);
+		Matrice<M>& operator-(const Matrice<M>& M);
+		Matrice<M>& operator*(int d);
+		Matrice<M>& operator*(Matrice<M>& M);
 		void print() const;
 		~Matrice();
 	};
+	template<class M>
+	Matrice<M>::Matrice(int lg, int col)
+	{
+		this->nbre_lgn = lg;
+		this->nbre_col = col;
+		tab = new M* [lg];
+		for (int j = 0; j < col; j++)
+		{
+			tab[j] = new int[col];
+		}
+
+		for (int i = 0; i < lg; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				this->tab[i][j] = 0;
+			}
+		}
+	}
+	template<class M>
+	void Matrice<M>::remplissage(M val)
+	{
+		for (int i = 0; i < this->nbre_lgn; i++)
+		{
+			for (int j = 0; j < this->nbre_col; j++)
+			{
+			   this->tab[i][j] = val;
+			}
+		}
+	}
+	template<class M>
+	void Matrice<M>::remplir()
+	{
+		for (int i = 0; i < this->nbre_lgn; i++)
+		{
+			cout << "ligne " << i + 1 << endl;
+			for (int j = 0; j < this->nbre_col; j++)
+			{
+				cout << "Matrice[" << i << "][" << j << "] = ";
+				cin >> this->tab[i][j];
+			}
+		}
+		cout << endl;
+	}
+	template<class M>
+	Matrice<M>& Matrice<M>::operator+(const Matrice<M>& M2)
+	{
+		Matrice<M>* M1 = new Matrice(this->nbre_lgn, this->nbre_col);
+		assert(M2.nbre_lgn == this->nbre_lgn && M2.nbre_col == this->nbre_col);
+		for (int i = 0; i < this->nbre_lgn; i++)
+		{
+			for (int j = 0; j < this->nbre_col; j++)
+			{
+				//cout << M.tab[i][j] << endl;
+				M1->tab[i][j] = this->tab[i][j] + M2.tab[i][j];
+				//cout << M1->tab[i][j] << endl;
+			}
+		}
+		return *M1;
+	}
+	template<class M>
+	Matrice<M>& Matrice<M>::operator-(const Matrice<M>& M2)
+	{
+		// TODO: insérer une instruction return ici
+		Matrice<M>* M1 = new Matrice(this->nbre_lgn, this->nbre_col);
+		assert(M2.nbre_lgn == this->nbre_lgn && M2.nbre_col == this->nbre_col);
+		for (int i = 0; i < this->nbre_lgn; i++)
+		{
+			for (int j = 0; j < this->nbre_col; j++)
+			{
+				//cout << M.tab[i][j] << endl;
+				M1->tab[i][j] = this->tab[i][j] - M2.tab[i][j];
+				//cout << M1->tab[i][j] << endl;
+			}
+		}
+		return *M1;
+	}
+	template<class M>
+	Matrice<M>& Matrice<M>::operator*(int d)
+	{
+		Matrice<M>* M1 = new Matrice(this->nbre_lgn, this->nbre_col);
+
+		for (int i = 0; i < this->nbre_lgn; i++)
+		{
+			for (int j = 0; j < this->nbre_col; j++)
+			{
+				(M1->tab[i][j]) = this->tab[i][j] * d;
+			}
+		}
+		return *M1;
+	}
+	template<class M>
+	Matrice<M>& Matrice<M>::operator*(Matrice<M>& M2)
+	{
+		Matrice<M>* M1 = new Matrice(this->nbre_lgn, this->nbre_col);
+		int res;
+		assert(M2.nbre_lgn == this->nbre_lgn && M2.nbre_col == this->nbre_col);
+		for (int i = 0; i < this->nbre_lgn; i++)
+			for (int j = 0; j < this->nbre_col; j++)
+			{
+				res = 0;
+				for (int k = 0; k < this->nbre_col; k++)
+					res += this->tab[i][k] * M2.tab[k][j];
+				M1->tab[i][j] = res;
+			}
+		return *M1;
+	}
+	template<class M>
+	void Matrice<M>::print() const
+	{
+		cout << "la matrice est : " << endl;
+		for (int i = 0; i < this->nbre_lgn; i++)
+		{
+			for (int j = 0; j < this->nbre_col; j++)
+			{
+				cout << " " << this->tab[i][j] << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	template<class M>
+	Matrice<M>::~Matrice()
+	{
+		for (int j = 0; j < this->nbre_col; j++)
+		{
+			delete(tab[j]);
+			this->tab[j] = 0;
+		}
+		delete(tab);
+		tab = 0;
+	}
 }
--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
     //Declaration des matrices
-    Matrice* M= new Matrice(2,2);
-    Matrice* M1 = new Matrice(2, 2);
+    Matrice<int>* M = new Matrice<int>(2, 2);
+    Matrice<int>* M1 = new Matrice<int>(2, 2);
 
     //Remplissages de matrices
     M->remplissage(1);
@@ -19,19 +19,19 @@ int main()
     M1->print();
 
     //Somme de deux matrices
-    Matrice M2=*M+*M1;
+    Matrice<int> M2 = *M + *M1;
     M2.print();
 
     //Soustraction de deux matrices
-    Matrice M3 = M2 - *M1;
+    Matrice<int> M3 = M2 - *M1;
     M3.print();
 
     //multiplication d'une matrice avect un entier
-    Matrice M4 = M2*2;
+    Matrice<int> M4 = M2 * 2;
     M4.print();
 
     //multiplication de deux matrices
-    Matrice M5 = (*M) * (*M);
+    Matrice<int> M5 = (*M) * (*M);
     M5.print();
 
 }
